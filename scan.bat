a56a85338f116500e7b9d901a0dbaf71e7d5c109
David-ZemaP <david.zema@ucb.edu.bo>
2026-03-14 23:30:26 -0400

    refactor: update .gitignore and remove obsolete scan.bat script

--- scan.bat
+++
@@ -1,5 +0,0 @@
-dotnet sonarscanner begin /k:"BookStoreManagement" /d:sonar.host.url="http://localhost:9000" /d:sonar.token="sqp_c0b00edb81ed69781d709406a41abbf9e2b00174"
-
-dotnet build
-
-dotnet sonarscanner end /d:sonar.token="sqp_c0b00edb81ed69781d709406a41abbf9e2b00174"
