89b8746a8d409f98aa76f5e24f4b596ffeae58de
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-04-17 23:40:38 -0400

    test: Se agrego pruebas path coverage para NormalizeSpaces en TextRules

---
+++ MicroServiceClient.Domain.UnitTest/TextRulesTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using MicroServiceClient.Domain.Validations;
+
+namespace MicroServiceClient.Domain.UnitTest
+{
+    public class TextRulesTest
+    {
+
+        [Theory]
+        [InlineData(null, "")]             // TC1: Caso nulo
+        [InlineData("   ", "")]             // TC1: Caso solo espacios
+        [InlineData("  hola   mundo  ", "hola mundo")] // TC2: Caso con espacios múltiples
+        public void NormalizeSpaces_PathCoverage_Tests(string input, string expected)
+        {
+            // Act
+            var result = TextRules.NormalizeSpaces(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
