ac0d507dc40dbdcf260810c945e88e3ef44a7a86
LokoAddams <lucasalcoba74@gmail.com>
2026-04-19 08:14:56 -0400

    refact: remove unused functions

---
+++ MicroServiceUsers.Domain.UnitTest/TextRulesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServiceUsers.Domain.UnitTest
+{
+    internal class TextRulesTests
+    {
+    }
+}
