799ad6c55e2229dd19187e5561cc4e44f2ff12a2
LokoAddams <lucasalcoba74@gmail.com>
2026-04-19 08:24:35 -0400

    tests: for TextRules from Users

--- MicroServiceUsers.Domain.UnitTest/TextRulesTests.cs
+++ MicroServiceUsers.Domain.UnitTest/TextRulesTests.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MicroServiceUsers.Domain.Validations;
 
 namespace MicroServiceUsers.Domain.UnitTest
 {
-    internal class TextRulesTests
+    public class TextRulesTests
     {
+        #region NormalizeSpaces Tests
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("  sistemas   ucb  ", "sistemas ucb")]
+        public void NormalizeSpaces_WithVariousInputs_ReturnsNormalizedString(string? input, string expected)
+        {
+            var result = TextRules.NormalizeSpaces(input);
+            Assert.Equal(expected, result);
+        }
+        #endregion
+
+        #region CanonicalPersonName Tests
+        [Theory]
+        [InlineData("   ", "")]
+        [InlineData(" luCAs  ", "Lucas")]
+        [InlineData("l", "L")]
+        public void CanonicalPersonName_WithVariousInputs_ReturnsCanonicalName(string? input, string expected)
+        {
+            var result = TextRules.CanonicalPersonName(input);
+            Assert.Equal(expected, result);
+        }
+        #endregion
+
+        #region IsValidLettersOnly Tests
+        [Theory]
+        [InlineData("", false)]
+        [InlineData("Bolivia", true)]
+        public void IsValidLettersOnly_WithVariousInputs_ReturnsExpectedResult(string? input, bool expected)
+        {
+            var result = TextRules.IsValidLettersOnly(input);
+            Assert.Equal(expected, result);
+        }
+        #endregion
+
+        #region IsValidLettersAndSpaces Tests
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("Sistemas Computacionales", true)]
+        public void IsValidLettersAndSpaces_WithVariousInputs_ReturnsExpectedResult(string? input, bool expected)
+        {
+            var result = TextRules.IsValidLettersAndSpaces(input);
+            Assert.Equal(expected, result);
+        }
+        #endregion
+
+        #region IsValidEmail Tests
+        [Theory]
+        [InlineData("   ", false)]
+        [InlineData("lucas@ucb.edu.bo", true)]
+        public void IsValidEmail_WithVariousInputs_ReturnsExpectedResult(string? input, bool expected)
+        {
+            var result = TextRules.IsValidEmail(input);
+            Assert.Equal(expected, result);
+        }
+        #endregion
+
+        #region IsValidUsername Tests
+        [Theory]
+        [InlineData("", false)]
+        [InlineData("lucas.alcoba_2026", true)]
+        public void IsValidUsername_WithVariousInputs_ReturnsExpectedResult(string? input, bool expected)
+        {
+            var result = TextRules.IsValidUsername(input);
+            Assert.Equal(expected, result);
+        }
+        #endregion
     }
 }
